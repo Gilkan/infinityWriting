c1b3343d7e9b6a7101c77e077849a430ba2ac037
Gilkan <33844408+Gilkan@users.noreply.github.com>
2021-02-26 14:58:58 -0300

    Update infinityWriting.js
    added activateKeyboardMapping directly in file

--- infinityWriting.js
+++ infinityWriting.js
@@ -1,6 +1,4 @@
-import {activateKeyboardMapping} from './keyboardMapping.js';
 function infinityWriting(id, fullscreen=false, speed=5, charPerSpeed = 1, backgroundColor='black', fontColor='green'){
-    
     let container = document.getElementById(id);
     container.style.background = backgroundColor;
     container.style.color = fontColor;
@@ -57,4 +55,52 @@ function decodeHTMLEntities(text) {
     var textArea = document.createElement('textarea');
     textArea.innerHTML = text;
     return textArea.value;
+}
+
+function activateKeyboardMapping(functionToExecuteOnMap=null){
+    //activating stop listener
+    var keyMapping = [];
+    document.addEventListener('keydown', function (event){
+        if(event.repeat){return}
+
+        keyMapping = toggleKeyMap(event.key, keyMapping);
+
+        //if exists functionToExecuteOnMap, run every press
+        if(functionToExecuteOnMap!=null){
+            functionToExecuteOnMap(keyMapping);
+        }
+    });
+    document.addEventListener('keyup', function (event){
+        if(event.repeat){return}
+        keyMapping = toggleKeyMap(event.key, keyMapping);
+    });
+
+    function toggleKeyMap(key, keymap, forceAction = null){
+        if(forceAction!=null){
+            switch (forceAction) {
+
+                case 'on':
+                case 1:
+                    keymap.push(key);
+                    break;
+
+                case 'off':
+                case 0:
+                    keymap.splice(keymap.indexOf(key),1);
+                    break;
+
+            }
+        } else {
+            if(keymap.includes(key)){ //se já tem, remover
+                keymap.splice(keymap.indexOf(key),1);
+            } else { //se não tem, incluir
+                keymap.push(key);
+            }
+        }
+
+
+        return keymap;
+    }
+
+    return keyMapping;
 }
